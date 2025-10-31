39978aee7844139e9572e239208b698111a1c269
NishantYadav9602 <your_email@example.com>
2025-10-31 08:10:56 +0000

    testing auto webhook build

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello Nishu — version 2\n');
+  res.end('Hello Nishant version 2\n');
 });
 
 const PORT = 3000;
@@ -12,3 +12,4 @@ server.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running at http://0.0.0.0:${PORT}/`);
 });
 
+console.log('Auto build triggered successfully ✅')
